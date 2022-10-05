4ac66b554941b360b37caf7312896c087c169dfb
ravi.prakash <ravi.0531.rp@gmail.com>
2022-10-05 23:47:40 +0530

    Grad Cam Script Added in scripts

---
+++ g-scripts/grad_cam.py
@@ -0,0 +1,50 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.cm as cm
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+def get_img_array(img_path, size):
+    img = keras.preprocessing.image.load_img(img_path, target_size=size)
+    array = keras.preprocessing.image.img_to_array(img)
+    array = np.expand_dims(array, axis=0)
+    return array
+
+def make_gradcam_heatmap(img_array, model, pred_index=None):
+    last_conv_layer_name = "block14_sepconv2_act"
+    grad_model = model
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
+
+def save_and_display_gradcam(img_path, heatmap,label, cam_path="cam.jpg", alpha=0.4):
+    img = keras.preprocessing.image.load_img(img_path)
+    img = keras.preprocessing.image.img_to_array(img)
+    heatmap = np.uint8(255 * heatmap)
+
+    jet = cm.get_cmap("jet")
+
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+    jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+
+    superimposed_img = jet_heatmap * alpha + img
+    superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+    superimposed_img.save(cam_path)
+    print("GradCAM with {}".format(label))
+    display(Image(cam_path))
